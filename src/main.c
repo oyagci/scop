211bb81f3bf8fb720d4466d56723cda546c0360d
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-01 20:27:54 +0100

    Add Movements

--- src/main.c
+++ src/main.c
@@ -17,6 +17,18 @@
 #define HEIGHT		600
 #define __unused	__attribute__((unused))
 
+// Keep track of aspect ratio changes when window is resized.
+// Set to `1' to force the first frame to set up the projections matrix.
+int		g_update_projection = 1;
+mat4	g_projection;
+
+vec3	*g_cam_pos_p;
+vec3	*g_cam_front_p;
+vec3	*g_cam_up_p;
+
+int		g_width = WIDTH;
+int		g_height = HEIGHT;
+
 GLfloat cube[] = {
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
      0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -63,20 +75,107 @@ GLfloat cube[] = {
 
 void framebufferResize(GLFWwindow __unused *win, int width, int height)
 {
+	g_width = width;
+	g_height = height;
+	g_update_projection = 1;
 	glViewport(0, 0, width, height);
 }
 
-vec3 g_cam_pos;
+double last_x = WIDTH / 2;
+double last_y = HEIGHT / 2;
+
+float pitch = 0;
+float yaw = -90.0f;
+int first_mouse = 1;
+ 
+void mouseMove(GLFWwindow *win, double xpos, double ypos)
+{
+	if (first_mouse) {
+		last_x = xpos;
+		last_y = ypos;
+		first_mouse = 0;
+	}
+
+	float xoff = xpos - last_x;
+	float yoff = last_y - ypos;
+
+	last_x = xpos;
+	last_y = ypos;
+
+	float sensitivity = 0.001f;
+	xoff *= sensitivity;
+	yoff *= sensitivity;
+
+	yaw += xoff;
+	pitch += yoff;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	vec3 front;
+	glm_vec3_copy((vec3){
+			cos(glm_rad(yaw)) * cos(glm_rad(pitch)),
+			sin(glm_rad(pitch)),
+			sin(glm_rad(yaw)) * cos(glm_rad(pitch)),
+		}, front);
+
+	glm_normalize(front);
+	glm_vec3_print(front, stderr);
+	glm_vec3_copy(front, *g_cam_front_p);
+}
+
+float g_delta_time = 0.0f;
 
 void processInput(GLFWwindow *win)
 {
+	float speed = 4.f * g_delta_time;
+
+	if (glfwGetKey(win, GLFW_KEY_ESCAPE)) {
+		glfwSetWindowShouldClose(win, GLFW_TRUE);
+	}
 	if (glfwGetKey(win, GLFW_KEY_W)) {
+		vec3 newpos = {
+			(*g_cam_front_p)[0] * speed,
+			(*g_cam_front_p)[1] * speed,
+			(*g_cam_front_p)[2] * speed
+		};
+
+		glm_vec3_add(*g_cam_pos_p, newpos, *g_cam_pos_p);
 	}
 	if (glfwGetKey(win, GLFW_KEY_S)) {
+		vec3 newpos = {
+			(*g_cam_front_p)[0] * -speed,
+			(*g_cam_front_p)[1] * -speed,
+			(*g_cam_front_p)[2] * -speed
+		};
+
+		glm_vec3_add(*g_cam_pos_p, newpos, *g_cam_pos_p);
 	}
 	if (glfwGetKey(win, GLFW_KEY_A)) {
+		vec3 right;
+
+		glm_vec3_cross(*g_cam_front_p, *g_cam_up_p, right);
+		glm_normalize(right);
+		glm_vec3_muladds(right, -speed, *g_cam_pos_p);
 	}
 	if (glfwGetKey(win, GLFW_KEY_D)) {
+		vec3 right;
+
+		glm_vec3_cross(*g_cam_front_p, *g_cam_up_p, right);
+		glm_normalize(right);
+		glm_vec3_muladds(right, speed, *g_cam_pos_p);
+	}
+	if (glfwGetKey(win, GLFW_KEY_SPACE)) {
+		vec3 newpos = { 0.0f, 1.0f * speed, 0.0f };
+
+		glm_vec3_add(*g_cam_pos_p, newpos, *g_cam_pos_p);
+	}
+	if (glfwGetKey(win, GLFW_KEY_LEFT_CONTROL)) {
+		vec3 newpos = { 0.0f, 1.0f * -speed, 0.0f };
+
+		glm_vec3_add(*g_cam_pos_p, newpos, *g_cam_pos_p);
 	}
 }
 
@@ -111,11 +210,11 @@ int	main(void)
 	GLFWwindow *window = NULL;
 
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(WIDTH, HEIGHT, "scop", NULL, NULL);
+	window = glfwCreateWindow(g_width, g_height, "scop", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Could not create window!\n");
 		glfwTerminate();
@@ -131,7 +230,10 @@ int	main(void)
 	}
 
 	glfwSetFramebufferSizeCallback(window, framebufferResize);
-	glViewport(0, 0, WIDTH, HEIGHT);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouseMove);
+
+	glViewport(0, 0, g_width, g_height);
 	glEnable(GL_DEPTH_TEST);
 
 	char *vertexShaderSource = NULL;
@@ -224,21 +326,15 @@ int	main(void)
 
 	mat4 trans;
 	mat4 model;
-	mat4 view;
 	mat4 persp;
 
 	glm_mat4_identity(trans);
 	glm_mat4_identity(model);
-	glm_mat4_identity(view);
 	glm_mat4_identity(persp);
 
 	glm_rotate(model, glm_rad(-55.0f), (vec3){ 1.0f, 0.0f, 0.0f });
 	glUniformMatrix4fv(glGetUniformLocation(shader1.index, "model"), 1,
 		GL_FALSE, model[0]);
-
-	glm_perspective(45.0f, (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f, persp);
-	glUniformMatrix4fv(glGetUniformLocation(shader1.index, "proj"), 1,
-		GL_FALSE, persp[0]);
 
 	vec3 cubePositions[] = {
 		{ 0.0f,  0.0f,  0.0f}, 
@@ -253,47 +349,64 @@ int	main(void)
 		{-1.3f,  1.0f, -1.5f}  
 	};
 
-//	vec3 cam_pos = { 0.0f, 0.0f, -3.0f };
-//	vec3 cam_target = { 0.0f, 0.0f, 0.0f };
-//
-//
-//	vec3 cam_dir;
-//	glm_vec3_sub(cam_pos, cam_target, cam_dir);
-//
-//
-//	vec3 cam_right;
-//	glm_cross((vec3){ 0.0f, 1.0f, 0.0f }, cam_dir, cam_right);
-//	glm_normalize(cam_right);
-//
-//
-//	vec3 cam_up;
-//	glm_cross(cam_dir, cam_right, cam_up);
-//
-//	glm_lookat((vec3){ 0.0f, 0.0f, 3.0f }, (vec3){ 0.0f, 0.0f, 0.0f },
-//		(vec3){ 0.0f, 1.0f, 0.0f }, view);
-
-//	glUniformMatrix4fv(glGetUniformLocation(shader1.index, "view"), 1,
-//		GL_FALSE, view[0]);
-
 	vec3 cam_front = { 0.0f, 0.0f, -1.0f };
 	vec3 cam_up = { 0.0f, 1.0f, 0.0f };
 	vec3 cam_pos = { 0.0f, 0.0f, 3.0f};
 
+	g_cam_front_p = &cam_front;
+	g_cam_pos_p = &cam_pos;
+	g_cam_up_p = &cam_up;
+
+	float delta_time = 0.0f;
+	float last_frame = 0.0f;
+	float total_time = 0.0f;
+	size_t nimg = 0;
 	while (!glfwWindowShouldClose(window)) {
+
+		float current_frame = glfwGetTime();
+
+		delta_time = current_frame - last_frame;
+		last_frame = current_frame;
+		g_delta_time = delta_time;
+
+		// FPS Counter
+		total_time += delta_time;
+		nimg += 1;
+		if (total_time >= 1.0f) {
+			fprintf(stderr, "\rFPS: %d", (int)(nimg / total_time));
+			total_time = 0.0f;
+			nimg = 0;
+		}
+
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		processInput(window);
+
+		// Rendering
+
+		if (g_update_projection) {
+			glm_perspective(45.0f, (float)g_width/(float)g_height, 0.1f, 100.0f,
+				g_projection);
+			glUniformMatrix4fv(glGetUniformLocation(shader1.index, "proj"), 1,
+				GL_FALSE, g_projection[0]);
+
+			g_update_projection = 0;
+		}
+
 		program_use(&shader1);
+
+		mat4 view;
 
 		vec3 cam_target;
 		glm_vec3_add(cam_front, cam_pos, cam_target);
 
 		glm_lookat(cam_pos, cam_target, cam_up, view);
-
 		glUniformMatrix4fv(glGetUniformLocation(shader1.index, "view"), 1,
 			GL_FALSE, view[0]);
 
-		for (size_t i = 0; i < sizeof(cubePositions) / sizeof(*cubePositions); i++) {
+		for (size_t i = 0; i < sizeof(cubePositions) / sizeof(*cubePositions);
+			i++) {
 			object_set_pos(&o1, cubePositions[i]);
 			object_rotx(&o1, 20.0f * i);
 			object_rotz(&o1, 20.0f * i);
