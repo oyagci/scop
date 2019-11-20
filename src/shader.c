cf6f9872e92f5425021ba263d67fa74196cbf86c
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 21:37:03 +0100

    Apply norme to shader.c

--- src/shader.c
+++ src/shader.c
@@ -78,25 +78,25 @@ void	program_delete(struct s_program *p)
 	glDeleteProgram(p->index);
 }
 
-void program_use(struct s_program *p)
+void	program_use(struct s_program *p)
 {
 	glUseProgram(p->index);
 }
 
-void shader_set_vec3(struct s_program *p, char const *name, vec3 v)
+void	shader_set_vec3(struct s_program *p, char const *name, vec3 v)
 {
 	program_use(p);
 	glUniform3fv(glGetUniformLocation(p->index, name), 1, v);
 }
 
-void shader_set_mat4(struct s_program *p, char const *name, mat4 m)
+void	shader_set_mat4(struct s_program *p, char const *name, mat4 m)
 {
 	program_use(p);
 	glUniformMatrix4fv(glGetUniformLocation(p->index, name), 1,
 		GL_FALSE, *m);
 }
 
-void shader_set_float(struct s_program *p, char const *name, float f)
+void	shader_set_float(struct s_program *p, char const *name, float f)
 {
 	program_use(p);
 	glUniform1f(glGetUniformLocation(p->index, name), f);
