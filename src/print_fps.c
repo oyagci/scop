317ff38925f988a25fce85d24d421d518f51e0af
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 21:20:04 +0100

    Apply norme to print_fps

--- src/print_fps.c
+++ src/print_fps.c
@@ -3,12 +3,13 @@
 
 void print_fps(void)
 {
-	static float total_time = 0.0f;
-	static size_t nimg = 0;
+	static float	total_time = 0.0f;
+	static size_t	nimg = 0;
 
 	total_time += g_engine.delta_time;
 	nimg += 1;
-	if (total_time >= 1.0f) {
+	if (total_time >= 1.0f)
+	{
 		fprintf(stderr, "\rFPS: %d", (int)(nimg / total_time));
 		total_time = 0.0f;
 		nimg = 0;
