3328772ac5179445683d2bc265b1740b62180ead
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 20:41:17 +0100

    Apply norme to obj_add_face

--- src/objparser.c
+++ src/objparser.c
@@ -185,33 +185,37 @@ int	scan_verts(unsigned int vert[3], char const *line)
 	return (0);
 }
 
+void	put_face_data(t_face_indices *inds, unsigned int vert[3])
+{
+	inds->vert = vert[0];
+	inds->norm = vert[1];
+	inds->text = vert[2];
+}
+
 int	obj_add_face(t_obj *obj, const char *data)
 {
-	t_face	face;
-	size_t	nverts;
-	unsigned int verts[4][3];
+	t_face			face;
+	size_t			nverts;
+	unsigned int	verts[4][3];
+	size_t			i;
 
 	memset(verts, 0, sizeof(verts));
-
 	nverts = 0;
-	while (nverts < 4) {
+	while (nverts < 4)
+	{
 		scan_verts(verts[nverts], data);
 		nverts++;
 		data = strchr(data, ' ');
-		if (!data) {
+		if (!data)
 			break ;
-		}
 		data++;
 	}
-
-	face.indices = malloc(sizeof(*face.indices) * nverts);
+	face.indices = malloc_abort(sizeof(*face.indices) * nverts);
 	face.nverts = nverts;
-
-	size_t i = 0;
-	while (i < nverts) {
-		face.indices[i].vert = verts[i][0];
-		face.indices[i].norm = verts[i][1];
-		face.indices[i].text = verts[i][2];
+	i = 0;
+	while (i < nverts)
+	{
+		put_face_data(face.indices + i, verts[i]);
 		i++;
 	}
 	face_container_add(&obj->faces, &face);
