4585a7227794af14d4e2ddbe8a941365ba343c2b
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 21:38:02 +0100

    Apply norme to object.h

--- inc/object.h
+++ inc/object.h
@@ -6,38 +6,38 @@
 # include "shader.h"
 # include "glm.h"
 
-struct s_object {
-	vec3			pos;
-	vec3			rot;
-	GLfloat			scale;
+struct	s_object {
+	vec3				pos;
+	vec3				rot;
+	GLfloat				scale;
 
-	GLuint			vbo;
-	GLuint			vao;
-	GLuint			ebo;
+	GLuint				vbo;
+	GLuint				vao;
+	GLuint				ebo;
 
-	GLfloat 		*vertices;
-	GLsizei			nvert;
+	GLfloat 			*vertices;
+	GLsizei				nvert;
 
-	GLuint			*indices;
-	GLsizei			nind;
+	GLuint				*indices;
+	GLsizei				nind;
 
-	struct s_program *shader;
+	struct s_program	*shader;
 };
 
-void	object_init(struct s_object *o,
-			GLfloat const *vdata,
-			GLsizei ndata,
-			GLsizei stride,
-			struct s_program *shader);
-void	object_prepare_draw(struct s_object *o);
-void	object_draw(struct s_object *o);
-void	object_delete(struct s_object *o);
-void	object_update(struct s_object *o);
+void					object_init(struct s_object *o,
+							GLfloat const *vdata,
+							GLsizei ndata,
+							GLsizei stride,
+							struct s_program *shader);
+void					object_prepare_draw(struct s_object *o);
+void					object_draw(struct s_object *o);
+void					object_delete(struct s_object *o);
+void					object_update(struct s_object *o);
 
-void	object_set_pos(struct s_object *o, vec3 newpos);
-void	object_rotx(struct s_object *o, float angle);
-void	object_roty(struct s_object *o, float angle);
-void	object_rotz(struct s_object *o, float angle);
-void	object_set_scale(struct s_object *o, float angle);
+void					object_set_pos(struct s_object *o, vec3 newpos);
+void					object_rotx(struct s_object *o, float angle);
+void					object_roty(struct s_object *o, float angle);
+void					object_rotz(struct s_object *o, float angle);
+void					object_set_scale(struct s_object *o, float angle);
 
 #endif
