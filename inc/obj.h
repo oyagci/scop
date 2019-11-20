e08ad55ad34f4fff4dd13d22e06662ee8d199b6e
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 21:08:32 +0100

    Apply norme to obj.h

--- inc/obj.h
+++ inc/obj.h
@@ -29,8 +29,9 @@ typedef struct	s_vertex_container
 	size_t		capacity;
 }				t_vertex_container;
 
-int	vertex_container_init(t_vertex_container *vc);
-int	vertex_container_add(t_vertex_container *vc, t_vertex *v);
+int				vertex_container_init(t_vertex_container *vc);
+int				vertex_container_add(t_vertex_container *vc,
+					t_vertex *v);
 
 typedef struct	s_face_indices {
 	size_t	vert;
@@ -50,8 +51,8 @@ typedef struct	s_face_container
 	size_t		capacity;
 }				t_face_container;
 
-int	face_container_init(t_face_container *vc);
-int	face_container_add(t_face_container *vc, t_face *v);
+int				face_container_init(t_face_container *vc);
+int				face_container_add(t_face_container *vc, t_face *v);
 
 typedef struct	s_triangle {
 	vec3	vert[3];
@@ -65,18 +66,17 @@ typedef struct	s_triangle_container
 	size_t		capacity;
 }				t_triangle_container;
 
-int	triangle_container_init(t_triangle_container *vc);
-int	triangle_container_add(t_triangle_container *vc, t_triangle *v);
+int				triangle_container_init(t_triangle_container *vc);
+int				triangle_container_add(t_triangle_container *vc, t_triangle *v);
 
 typedef struct	s_obj
 {
-	char				*data;
+	char					*data;
 
-	t_vertex_container	vertices;
-	t_face_container	faces;
+	t_vertex_container		vertices;
+	t_face_container		faces;
 	t_triangle_container	triangles;
 }				t_obj;
-
 
 /*
 ** Special structures for OpenGL which expects data in an array of numbers
@@ -108,6 +108,6 @@ float			*obj_get_vertices(t_obj *obj);
 void			obj_triangulate(t_obj *obj);
 t_gltri			*obj_get_triangles_arr(t_obj *obj);
 
-int	read_file(char const *const filename, char **buf);
+int				read_file(char const *const filename, char **buf);
 
 #endif
