ab6d920ff0e0ab88ca9293d3ef3a336e28a459e5
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 21:35:13 +0100

    Apply norme to shader_create

--- src/shader.c
+++ src/shader.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "shader.h"
+#include <stdio.h>
 
 void	shader_init(struct s_shader *s, GLenum shader_type)
 {
@@ -42,42 +43,32 @@ void	program_init(struct s_program *p)
 	p->index = glCreateProgram();
 }
 
-#include <stdio.h>
 void	program_create(struct s_program *p,
-		const char *const vertsrc,
-		const char *const fragsrc)
+	const char *const vertsrc,
+	const char *const fragsrc)
 {
+	char log[512];
+
 	program_init(p);
 	shader_init(&p->vertex, GL_VERTEX_SHADER);
 	shader_init(&p->fragment, GL_FRAGMENT_SHADER);
-
 	shader_load_source(&p->vertex, vertsrc);
 	shader_compile(&p->vertex);
-
 	if (!shader_compile_success(&p->vertex))
 	{
-		char log[512];
-
 		shader_infolog(&p->vertex, log, sizeof(log));
 		fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", log);
 	}
-
 	shader_load_source(&p->fragment, fragsrc);
 	shader_compile(&p->fragment);
-
 	if (!shader_compile_success(&p->fragment))
 	{
-		char log[512];
-
 		shader_infolog(&p->fragment, log, sizeof(log));
 		fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", log);
 	}
-
 	glAttachShader(p->index, p->vertex.index);
 	glAttachShader(p->index, p->fragment.index);
-
 	glLinkProgram(p->index);
-
 	shader_delete(&p->vertex);
 	shader_delete(&p->fragment);
 }
