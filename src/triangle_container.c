b5f9d25793286c6a0f7c87f836ee516d00ecbe0c
Oguzhan YAGCI <oyagci@student.42.fr>
2019-11-20 20:57:48 +0100

    Apply norme to triangle_container_add

--- src/triangle_container.c
+++ src/triangle_container.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "obj.h"
+#include "common.h"
 
 int	triangle_container_init(t_triangle_container *vc)
 {
 	vc = calloc(500, sizeof(t_triangle));
-	if (vc) {
+	if (vc)
+	{
 		vc->capacity = 500;
 		vc->size = 0;
 		return (0);
@@ -16,15 +18,12 @@ int	triangle_container_add(t_triangle_container *vc, t_triangle *v)
 {
 	t_triangle	*d;
 
-	if (vc->size >= vc->capacity) {
-		d = realloc(vc->data, sizeof(t_triangle) * (vc->capacity + 500));
-		if (d) {
-			vc->data = d;
-			vc->capacity += 500;
-		}
-		else {
-			return (-1);
-		}
+	if (vc->size >= vc->capacity)
+	{
+		d = realloc_abort(vc->data,
+			sizeof(t_triangle) * (vc->capacity + 500));
+		vc->data = d;
+		vc->capacity += 500;
 	}
 	memcpy(vc->data + vc->size, v, sizeof(t_triangle));
 	vc->size += 1;
